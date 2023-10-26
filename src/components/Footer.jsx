9c8ed207e65ebb0863ae6b8bd18fb0902b6c334f
Muhammad Awan Ardy Firmansyah <94277917+AwanArdy@users.noreply.github.com>
2023-10-27 00:03:23 +0800

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className='notes-app__footer'>
+            <p>made with 努力, 感謝, 笑顔 by Muhammad Awan Ardy Firmansyah</p>
+        </div>
+    )
+}
+
+export default Footer;
